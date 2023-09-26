3a445e4e78a72ecbeac5ff327967eb0cda4418a3
swathyganapathi <programhero111@gmail.com>
2023-09-27 00:04:39 +0530

    new code is added

---
+++ add.py
@@ -0,0 +1,6 @@
+#datas uploaded from person 1 
+apple = 200
+orange = 300
+banana = 100
+print(apple+banana+orange)
+
---
+++ add2.py
@@ -0,0 +1,5 @@
+#details added by person2
+rose = 100
+jasmine = 200
+lotus = 300
+banana = 500
